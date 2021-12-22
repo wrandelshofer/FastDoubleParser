37fb0a624f276649e741ce5824fe99f5421d5e2e
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2021-12-22 11:01:45 +0100

    Adds test with double numbers that are hard to parse.

--- test/junit5/java/ch/randelshofer/fastdoubleparser/AbstractHandPickedTest.java
+++ test/junit5/java/ch/randelshofer/fastdoubleparser/AbstractHandPickedTest.java
@@ -174,12 +174,13 @@ abstract class AbstractHandPickedTest {
     @TestFactory
     List<DynamicNode> dynamicTestsNumbersThatCausedJavaToHang() {
         return Arrays.asList(
-                dynamicTest("2.2250738585072012e-308", () -> testLegalInput("2.2250738585072012e-308", 2.2250738585072012e-308)),
+                dynamicTest("2.2250738585072012e-308 (Java got stuck, oscillating between 0x1p-1022 and 0x0.fffffffffffffp-1022)", () -> testLegalInput("2.2250738585072012e-308", 2.2250738585072012e-308)),
                 dynamicTest("0.00022250738585072012e-304 (decimal point placement)", () -> testLegalInput("0.00022250738585072012e-304", 0.00022250738585072012e-304)),
                 dynamicTest("00000000002.2250738585072012e-308 (leading zeros)", () -> testLegalInput("00000000002.2250738585072012e-308", 00000000002.2250738585072012e-308)),
                 dynamicTest("2.225073858507201200000e-308 (trailing zeros)", () -> testLegalInput("2.225073858507201200000e-308", 2.225073858507201200000e-308)),
                 dynamicTest("2.2250738585072012e-00308 (leading zeros in the exponent)", () -> testLegalInput("2.2250738585072012e-00308", 2.2250738585072012e-00308)),
-                dynamicTest("2.2250738585072012997800001e-308 (superfluous digits beyond digit 17)", () -> testLegalInput("2.2250738585072012997800001e-308 ", 2.2250738585072012997800001e-308))
+                dynamicTest("2.2250738585072012997800001e-308 (superfluous digits beyond digit 17)", () -> testLegalInput("2.2250738585072012997800001e-308", 2.2250738585072012997800001e-308)),
+                dynamicTest("6.917529027641081856e+18 (C# rounding bug)", () -> testLegalInput("6.917529027641081856e+18", 6.917529027641081856e+18))
         );
     }
 
