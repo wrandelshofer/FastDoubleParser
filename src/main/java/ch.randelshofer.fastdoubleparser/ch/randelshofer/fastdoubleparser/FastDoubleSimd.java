6a8751d12771e0f8951908b9a55912101c6aa677
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2022-04-02 08:59:54 +0200

    Removes code that uses the incubating Java Vector API.

--- src/main/java/ch.randelshofer.fastdoubleparser/ch/randelshofer/fastdoubleparser/FastDoubleSimd.java
+++ src/main/java/ch.randelshofer.fastdoubleparser/ch/randelshofer/fastdoubleparser/FastDoubleSimd.java
@@ -5,20 +5,9 @@
 
 package ch.randelshofer.fastdoubleparser;
 
-import jdk.incubator.vector.ByteVector;
-import jdk.incubator.vector.IntVector;
-import jdk.incubator.vector.LongVector;
-import jdk.incubator.vector.ShortVector;
-import jdk.incubator.vector.VectorMask;
-
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
 import java.nio.ByteOrder;
-
-import static jdk.incubator.vector.VectorOperators.ADD;
-import static jdk.incubator.vector.VectorOperators.LSHL;
-import static jdk.incubator.vector.VectorOperators.UNSIGNED_GT;
-import static jdk.incubator.vector.VectorOperators.UNSIGNED_LT;
 
 /**
  * This class provides the mathematical functions needed by {@link FastDoubleParser}.
@@ -54,47 +43,6 @@ class FastDoubleSimd {
             MethodHandles.byteArrayViewVarHandle(long[].class, ByteOrder.LITTLE_ENDIAN);
     public final static VarHandle readLongFromByteArrayBigEndian =
             MethodHandles.byteArrayViewVarHandle(long[].class, ByteOrder.BIG_ENDIAN);
-    private static final IntVector POWERS_OF_10 = IntVector.fromArray(IntVector.SPECIES_256,
-            new int[]{1000_0000, 100_0000, 10_0000, 10000, 1000, 100, 10, 1}, 0);
-    private static final IntVector POWERS_OF_16_SHIFTS = IntVector.fromArray(IntVector.SPECIES_256,
-            new int[]{28, 24, 20, 16, 12, 8, 4, 0}, 0);
-
-    public static int tryToParseEightDigitsUtf16Vector(char[] a, int offset) {
-        ShortVector vec = ShortVector.fromCharArray(ShortVector.SPECIES_128, a, offset)
-                .sub((short) '0');
-        // With an unsigned gt we only need to check for > 9
-        if (vec.compare(UNSIGNED_GT, 9).anyTrue()) {
-            return -1;
-        }
-        return (int) vec
-                .castShape(IntVector.SPECIES_256, 0)
-                .mul(POWERS_OF_10)
-                .reduceLanesToLong(ADD);
-    }
-
-    /**
-     * Tries to parse eight digits at once.
-     *
-     * @param first  the first four characters
-     * @param second the second four characters
-     * @return the parsed digits or -1
-     */
-    public static long tryToParseEightDigitsUtf16Vector(long first, long second) {
-        ShortVector vec = LongVector.zero(LongVector.SPECIES_128)
-                .withLane(0, first)
-                .withLane(1, second)
-                .reinterpretAsShorts()
-                .sub((short) '0');
-
-        // With an unsigned gt we only need to check for > 9
-        if (vec.compare(UNSIGNED_GT, 9).anyTrue()) {
-            return -1L;
-        }
-        return vec
-                .castShape(IntVector.SPECIES_256, 0)
-                .mul(POWERS_OF_10)
-                .reduceLanesToLong(ADD);
-    }
 
     /**
      * Tries to parse eight digits at once.
@@ -120,19 +68,6 @@ class FastDoubleSimd {
         sval = 100 * (sval & 0xff) + (sval >>> 32);
 
         return sval + 10000 * fval;
-    }
-
-    static int tryToParseEightDigitsUtf8Vector(byte[] a, int offset) {
-        ByteVector vec = ByteVector.fromArray(ByteVector.SPECIES_64, a, offset)
-                .sub((byte) '0');
-        // With an unsigned gt we only need to check for > 9
-        if (vec.compare(UNSIGNED_GT, 9).anyTrue()) {
-            return -1;
-        }
-        return (int) vec
-                .castShape(IntVector.SPECIES_256, 0)
-                .mul(POWERS_OF_10)
-                .reduceLanesToLong(ADD);
     }
 
     static int tryToParseEightDigitsUtf8Swar(byte[] str, int offset) {
@@ -163,48 +98,6 @@ class FastDoubleSimd {
         val = (((val & mask) * 0x000F4240_00000064L)//100 + (1000000 << 32)
                 + (((val >>> 16) & mask) * 0x00002710_00000001L)) >>> 32;// 1 + (10000 << 32)
         return (int) val;
-    }
-
-    public static long tryToParseEightHexDigitsUtf16Vector(char[] a, int offset) {
-        ShortVector vec = ShortVector.fromCharArray(ShortVector.SPECIES_128, a, offset)
-                .sub((short) '0');
-        VectorMask<Short> gt9Msk;
-        // With an unsigned gt we only need to check for > 'f' - '0'
-        if (vec.compare(UNSIGNED_GT, 'f' - '0').anyTrue()
-                || (gt9Msk = vec.compare(UNSIGNED_GT, '9' - '0')).and(vec.compare(UNSIGNED_LT, 'a' - '0')).anyTrue()) {
-            return -1L;
-        }
-        return vec
-                .sub((short) ('a' - '0' - 10), gt9Msk)
-                .castShape(IntVector.SPECIES_256, 0)
-                .lanewise(LSHL, POWERS_OF_16_SHIFTS)
-                .reduceLanesToLong(ADD) & 0xffffffffL;
-    }
-
-
-    /**
-     * Tries to parse eight hex digits from a byte array using the
-     * Java vector API.
-     *
-     * @param a      contains 8 ascii characters
-     * @param offset the offset of the first character in {@code a}
-     *               returns -1 if {@code value} does not contain 8 ascii digits
-     */
-    static long tryToParseEightHexDigitsUtf8Vector(byte[] a, int offset) {
-        ByteVector vec = ByteVector.fromArray(ByteVector.SPECIES_64, a, offset)
-                .sub((byte) '0');
-        VectorMask<Byte> gt9Msk;
-        // With an unsigned gt we only need to check for > 'f' - '0'
-        if (vec.compare(UNSIGNED_GT, 'f' - '0').anyTrue()
-                || (gt9Msk = vec.compare(UNSIGNED_GT, '9' - '0'))
-                .and(vec.compare(UNSIGNED_LT, 'a' - '0')).anyTrue()) {
-            return -1L;
-        }
-        return vec
-                .sub((byte) ('a' - '0' - 10), gt9Msk)
-                .castShape(IntVector.SPECIES_256, 0)
-                .lanewise(LSHL, POWERS_OF_16_SHIFTS)
-                .reduceLanesToLong(ADD) & 0xffffffffL;
     }
 
     /**
