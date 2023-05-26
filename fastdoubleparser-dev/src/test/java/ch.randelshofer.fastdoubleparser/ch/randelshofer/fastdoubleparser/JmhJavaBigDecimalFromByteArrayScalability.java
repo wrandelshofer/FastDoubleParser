75348507775e465f97cfc35cebbb4039491afbd9
Werner Randelshofer <werner.randelshofer@fibermail.ch>
2023-05-26 08:13:21 +0200

    Using the fix for https://bugs.openjdk.org/browse/JDK-8205592 in slow path does not make parsing faster. So we keep falling back on Double.parseDouble for slow path.

--- fastdoubleparser-dev/src/test/java/ch.randelshofer.fastdoubleparser/ch/randelshofer/fastdoubleparser/JmhJavaBigDecimalFromByteArrayScalability.java
+++ fastdoubleparser-dev/src/test/java/ch.randelshofer.fastdoubleparser/ch/randelshofer/fastdoubleparser/JmhJavaBigDecimalFromByteArrayScalability.java
@@ -4,18 +4,7 @@
  */
 package ch.randelshofer.fastdoubleparser;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.*;
 
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
@@ -27,14 +16,35 @@ import static ch.randelshofer.fastdoubleparser.Strings.repeat;
  * Benchmarks for selected floating point strings.
  * <pre>
  * # JMH version: 1.36
- * # VM version: JDK 20-ea, OpenJDK 64-Bit Server VM, 20+36-2344
+ * # VM version: JDK 21-ea, OpenJDK 64-Bit Server VM, 21-ea+20-1677
  * # Intel(R) Core(TM) i7-8700B CPU @ 3.20GHz
  *
- *      (digits)  Mode  Cnt              Score   Error  Units
- * m    10000000  avgt         1_837172513.667          ns/op
- * m   100000000  avgt        22_068128637.000          ns/op
- * m   646456993  avgt       202_281998613.000          ns/op
- * m  1292782621  avgt       212_085007186.000          ns/op
+ * via Double.parseDouble
+ *    (digits)  Mode  Cnt     Score   Error  Units
+ * m         1  avgt         26.139          ns/op
+ * m        10  avgt         31.454          ns/op
+ * m       100  avgt        244.925          ns/op
+ * m       200  avgt        506.917          ns/op
+ * m       300  avgt        912.334          ns/op
+ * m       400  avgt       1365.712          ns/op
+ * m       500  avgt       2082.934          ns/op
+ * m       600  avgt       2902.877          ns/op
+ * m       700  avgt       3537.485          ns/op
+ * m       800  avgt       4446.817          ns/op
+ *
+ * via BigDecimal:
+ *
+ *    (digits)  Mode  Cnt     Score   Error  Units
+ * m         1  avgt         25.626          ns/op
+ * m        10  avgt         31.454          ns/op
+ * m       100  avgt        255.092          ns/op
+ * m       200  avgt        506.333          ns/op
+ * m       300  avgt        887.673          ns/op
+ * m       400  avgt       1423.838          ns/op
+ * m       500  avgt       2114.062          ns/op
+ * m       600  avgt       2928.280          ns/op
+ * m       700  avgt       3567.702          ns/op
+ * m       800  avgt       4362.583          ns/op
  * </pre>
  */
 
@@ -46,24 +56,30 @@ import static ch.randelshofer.fastdoubleparser.Strings.repeat;
         //       ,"-XX:+UnlockDiagnosticVMOptions", "-XX:PrintAssemblyOptions=intel", "-XX:CompileCommand=print,ch/randelshofer/fastdoubleparser/JavaBigDecimalParser.*"
 
 })
-@Measurement(iterations = 2)
-@Warmup(iterations = 2)
+@Measurement(iterations = 1)
+@Warmup(iterations = 1)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode(Mode.AverageTime)
 @State(Scope.Benchmark)
 public class JmhJavaBigDecimalFromByteArrayScalability {
     @Param({
-            // "1"
-            // , "10"
-            // , "100"
-            // , "1000"
+            "1"
+            , "10"
+            , "100"
+            , "200"
+            , "300"
+            , "400"
+            , "500"
+            , "600"
+            , "700"
+            , "800"
             // , "10000"
             // , "100000"
             // , "1000000"
-            "10000000"
-            , "100000000"
-            , "646456993"
-            , "1292782621"
+            //,"10000000"
+            //, "100000000"
+            //, "646456993"
+            //, "1292782621"
 
     })
     public int digits;
