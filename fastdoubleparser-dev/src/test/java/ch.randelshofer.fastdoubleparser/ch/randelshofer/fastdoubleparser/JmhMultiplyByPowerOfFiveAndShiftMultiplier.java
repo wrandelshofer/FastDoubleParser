947e66b88c720ab757f0307a9f902c6d37de14a3
xtonik <antonin.janec@gmail.com>
2023-06-16 11:07:17 +0200

    handle bit shifts simply outside

--- fastdoubleparser-dev/src/test/java/ch.randelshofer.fastdoubleparser/ch/randelshofer/fastdoubleparser/JmhMultiplyByPowerOfFiveAndShiftMultiplier.java
+++ fastdoubleparser-dev/src/test/java/ch.randelshofer.fastdoubleparser/ch/randelshofer/fastdoubleparser/JmhMultiplyByPowerOfFiveAndShiftMultiplier.java
@@ -11,6 +11,29 @@ import java.math.BigInteger;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Benchmarks for selected floating point strings.
+ * <pre>
+ * # JMH version: 1.36
+ * # VM version: JDK 20.0.1, OpenJDK 64-Bit Server VM, 20.0.1+9-29
+ * # Intel(R) Core(TM) i5-6200U CPU @ 2.30GHz
+ *
+ * Benchmark  (bits)  Mode  Cnt       Score      Error  Units
+ * optimized     700  avgt    4     250.376 ±    3.773  ns/op
+ * optimized     800  avgt    4     317.921 ±   30.518  ns/op
+ * optimized     900  avgt    4     405.584 ±    2.953  ns/op
+ * optimized    2000  avgt    4    1371.457 ±   15.372  ns/op
+ * optimized   10000  avgt    4   30015.376 ± 1073.798  ns/op
+ * optimized   30000  avgt    4  180735.583 ±  825.962  ns/op
+ * original      700  avgt    4     254.843 ±    1.414  ns/op
+ * original      800  avgt    4     339.564 ±    2.898  ns/op
+ * original      900  avgt    4     463.033 ±    4.538  ns/op
+ * original     2000  avgt    4    1828.848 ±  121.480  ns/op
+ * original    10000  avgt    4   33663.669 ± 3189.014  ns/op
+ * original    30000  avgt    4  204170.758 ± 3690.179  ns/op
+ * </pre>
+ */
+
 @Fork(value = 1, jvmArgsAppend = {
         "-XX:+UnlockExperimentalVMOptions", "--add-modules", "jdk.incubator.vector"
         , "--enable-preview"
@@ -24,8 +47,8 @@ public class JmhMultiplyByPowerOfFiveAndShiftMultiplier {
 
 
     @Param({
-            "700"
-            , "800" // above this value, variant with bit shift leads
+            "700" // above this value, variant with bit shift leads
+            , "800"
             , "900"
             , "2000"
             , "10000"
@@ -34,6 +57,7 @@ public class JmhMultiplyByPowerOfFiveAndShiftMultiplier {
     public int bits;
     private BigInteger a;
     private BigInteger b;
+    private BigInteger bs;
     public int zeroes;
 
     @Setup(Level.Trial)
@@ -58,6 +82,7 @@ public class JmhMultiplyByPowerOfFiveAndShiftMultiplier {
 
         a = new BigInteger(1, bytesA);
         b = new BigInteger(1, bytesB);
+        bs = b.shiftRight(zeroes * 8); // preshift value - imitate 5^n
         System.out.println(b.getLowestSetBit() + " bits from " + length * 8 + " in total are zero");
     }
 
@@ -69,32 +94,8 @@ public class JmhMultiplyByPowerOfFiveAndShiftMultiplier {
 
     @Benchmark
     public void optimized(Blackhole blackhole) {
-        blackhole.consume(multiply(a, b, zeroes * 8));
+        blackhole.consume(FftMultiplier.multiply(a, bs).shiftLeft(zeroes * 8));
     }
-
-    // early identical copy of existing method FftMultiplier.multiply() to imitate real performance gain
-    static BigInteger multiply(BigInteger a, BigInteger b, int shift) {
-        if (b.signum() == 0 || a.signum() == 0) {
-            return BigInteger.ZERO;
-        }
-
-        int xlen = a.bitLength();
-        int ylen = b.bitLength();
-        if ((long) xlen + ylen > 32L * MAX_MAG_LENGTH) {
-            throw new ArithmeticException("BigInteger would overflow supported range");
-        }
-
-        if (xlen > TOOM_COOK_THRESHOLD
-                && ylen > TOOM_COOK_THRESHOLD
-                && (xlen > FFT_THRESHOLD || ylen > FFT_THRESHOLD)) {
-            throw new IllegalStateException("should not be tested here");
-        }
-        return a.multiply(b.shiftRight(shift)).shiftLeft(shift);
-    }
-
-    private static final int FFT_THRESHOLD = 33220;
-    private static final int MAX_MAG_LENGTH = Integer.MAX_VALUE / Integer.SIZE + 1; // (1 << 26)
-    private static final int TOOM_COOK_THRESHOLD = 240 * 8;
 }
 
 
